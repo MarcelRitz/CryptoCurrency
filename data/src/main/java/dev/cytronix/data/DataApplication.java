def8b9adeab8d7eade0f86ce663aeaa453bb3116
Marcel Ritz <mritz@sunrise.ch>
2017-09-16 23:11:18 +0200

    Crashlytics added

---
+++ data/src/main/java/dev/cytronix/data/DataApplication.java
@@ -0,0 +1,8 @@
+package dev.cytronix.data;
+
+import android.annotation.SuppressLint;
+import android.app.Application;
+
+@SuppressLint("Registered")
+public class DataApplication extends Application {
+}
