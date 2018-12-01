91b9552454607a34aa021f0defd5141b88854b28
Marcel Ritz <mritz@sunrise.ch>
2018-12-01 20:42:50 +0100

    Solve lint checker warnings

--- wear/src/main/java/dev/cytronix/cryptocurrency/binding/BindingAdapter.java
+++ wear/src/main/java/dev/cytronix/cryptocurrency/binding/BindingAdapter.java
@@ -9,13 +9,13 @@ import dev.cytronix.data.cryptowat.model.Price;
 
 public class BindingAdapter {
 
-    @androidx.databinding.BindingAdapter("android:visible")
+    @androidx.databinding.BindingAdapter("visible")
     public static void setVisible(View view, boolean visible) {
         view.setVisibility((visible) ? View.VISIBLE : View.GONE);
     }
 
     @SuppressLint("SetTextI18n")
-    @androidx.databinding.BindingAdapter({"android:price", "android:currency"})
+    @androidx.databinding.BindingAdapter({"price", "currency"})
     public static void setPrice(TextView textView, double price, String currency) {
         textView.setText((Price.PRICE_DEFAULT == price) ? "-" : PriceUtils.format(price) + currency);
     }
